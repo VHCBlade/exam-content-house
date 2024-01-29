9c924523b97da06342d4298118fc9a2519404406
Vincent Haron C. Mamutuk <vhcblade@gmail.com>
2024-01-29 22:05:21 +0800

    Task 1: Added l10n.yaml file to allow for necessary code generation

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class App extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''), // English, no country code
+            Locale('en', ''), // English, no country codeconst Locale('en', ''),
           ],
 
           // route controls
